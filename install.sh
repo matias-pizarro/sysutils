acd14de9f0b5ba482a70ef55b7970ee17b20a169
matias pizarro <matias@pizarro.net>
2024-09-30 11:53:14 +0200

    Initial commit

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+/usr/bin/install -v -m 0755 ./bin/* /usr/local/bin
+echo ""
---
+++ uninstall.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+rm -f /usr/local/bin/acpi_cmbat
+echo "deleted: /usr/local/bin/acpi_cmbat"
+rm -f /usr/local/bin/ctrl
+echo "deleted: /usr/local/bin/ctrl"
+rm -f /usr/local/bin/desktop-pause
+echo "deleted: /usr/local/bin/desktop-pause"
+rm -f /usr/local/bin/herbe1
+echo "deleted: /usr/local/bin/herbe1"
+rm -f /usr/local/bin/herbe2
+echo "deleted: /usr/local/bin/herbe2"
+rm -f /usr/local/bin/herbe4
+echo "deleted: /usr/local/bin/herbe4"
+rm -f /usr/local/bin/herbe8
+echo "deleted: /usr/local/bin/herbe8"
+rm -f /usr/local/bin/herbe60
+echo "deleted: /usr/local/bin/herbe60"
+rm -f /usr/local/bin/pwroff
+echo "deleted: /usr/local/bin/pwroff"
+rm -f /usr/local/bin/reset_wlan0
+echo "deleted: /usr/local/bin/reset_wlan0"
+rm -f /usr/local/bin/suspend
+echo "deleted: /usr/local/bin/suspend"
+echo ""
