c6afdf42db1f3d86a24e247f0532b0c319506338
matias pizarro <matias@pizarro.net>
2025-05-28 15:49:15 +0200

    update acpi_cmbat and ctrl

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-/usr/bin/install -v -m 0755 ./bin/* /usr/local/bin
+binaries="acpi_cmbat ctrl desktop-pause herbe1 herbe2 herbe4 herbe60 herbe8 pwroff reset_wlan0 suspend"
+
+echo ""
+for _util in ${binaries};do
+	/usr/bin/install -v -m 0755 ./bin/${_util} /usr/local/bin
+done
 echo ""
--- uninstall.sh
+++ uninstall.sh
@@ -1,25 +1,9 @@
 #!/bin/sh
 
-rm -f /usr/local/bin/acpi_cmbat
-echo "deleted: /usr/local/bin/acpi_cmbat"
-rm -f /usr/local/bin/ctrl
-echo "deleted: /usr/local/bin/ctrl"
-rm -f /usr/local/bin/desktop-pause
-echo "deleted: /usr/local/bin/desktop-pause"
-rm -f /usr/local/bin/herbe1
-echo "deleted: /usr/local/bin/herbe1"
-rm -f /usr/local/bin/herbe2
-echo "deleted: /usr/local/bin/herbe2"
-rm -f /usr/local/bin/herbe4
-echo "deleted: /usr/local/bin/herbe4"
-rm -f /usr/local/bin/herbe8
-echo "deleted: /usr/local/bin/herbe8"
-rm -f /usr/local/bin/herbe60
-echo "deleted: /usr/local/bin/herbe60"
-rm -f /usr/local/bin/pwroff
-echo "deleted: /usr/local/bin/pwroff"
-rm -f /usr/local/bin/reset_wlan0
-echo "deleted: /usr/local/bin/reset_wlan0"
-rm -f /usr/local/bin/suspend
-echo "deleted: /usr/local/bin/suspend"
+binaries="acpi_cmbat ctrl desktop-pause herbe1 herbe2 herbe4 herbe60 herbe8 pwroff reset_wlan0 suspend"
+
+for _util in ${binaries};do
+	rm -f /usr/local/bin/${_util}
+	echo "deleted: /usr/local/bin/${_util}";
+done
 echo ""
